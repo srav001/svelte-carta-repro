749dfdba721a642204c40009b9eaf30f681f7e63
Sravan <sravansuresh1x@gmail.com>
2024-06-19 16:11:28 +0530

    feat: init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Carta, MarkdownEditor } from "carta-md";
+	import "carta-md/default.css";
+
+	const carta = new Carta({
+		sanitizer: (html: string) => html,
+		extensions: [],
+	});
+
+	let value = "";
+</script>
+
+<main>
+	<MarkdownEditor {carta} bind:value />
+</main>
+
+<style>
+</style>
